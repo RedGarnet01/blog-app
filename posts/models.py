4ee765b88040e49dc47497e507e2aefc09acca1e
Jovan Dandridge <quantumcoder01@gmail.com>
2019-04-16 17:01:13 -0700

    Working Blog page, added search feature

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -22,7 +23,7 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     # content = HTMLField()
     comment_count = models.IntegerField(default = 0)
-    # view_count = models.IntegerField(default = 0)
+    view_count = models.IntegerField(default = 0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
@@ -32,3 +33,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={
+            'id': self.id
+        })
